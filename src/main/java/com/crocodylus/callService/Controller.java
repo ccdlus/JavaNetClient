3ce569843a5aec196b08262b4e71d2ea65d69303
ccdlus <crocodylus_bell@hotmail.com>
2017-08-12 16:32:15 +0700

    add how to call post

--- src/main/java/com/crocodylus/callService/Controller.java
+++ src/main/java/com/crocodylus/callService/Controller.java
@@ -3,15 +3,20 @@ package com.crocodylus.callService;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Map;
 
 import javax.management.RuntimeErrorException;
 
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 public class Controller {
@@ -20,19 +25,19 @@ public class Controller {
 		return "Hello";
 	}
 
-	@RequestMapping(value="net")
-	public void testNet() {
+	@RequestMapping(value = "netGet")
+	public void netGet() {
 		try {
 
 			URL url = new URL("https://jsonplaceholder.typicode.com/posts/1");
-			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 			conn.setRequestMethod("GET");
 			conn.setRequestProperty("Accept", "application/json");
-			
+
 			if (conn.getResponseCode() != 200) {
 				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
 			}
-			
+
 			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
 			String output;
@@ -41,6 +46,83 @@ public class Controller {
 				System.out.println(output);
 			}
 			conn.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	@RequestMapping(value = "netPost")
+	public void netPost() {
+		try {
+			URL url = new URL("https://jsonplaceholder.typicode.com/posts");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Content-Type", "application/json");
+			
+			String input = "{\"title\":\"test title\",\"body\":\"test body\",\"userId\":1}";
+
+			OutputStream os = conn.getOutputStream();
+			os.write(input.getBytes());
+			os.flush();
+
+			if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+			String output;
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+
+			conn.disconnect();
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@RequestMapping(value = "netPost2")
+	public void netPost2() {
+		try {
+			URL url = new URL("https://jsonplaceholder.typicode.com/posts");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Content-Type", "application/json");
+			
+//			String input = "{\"title\":\"test title\",\"body\":\"test body\",\"userId\":1}";
+			PostData postData = new PostData("test title", "test body", 1);
+			ObjectMapper mapper = new ObjectMapper();
+			String input = mapper.writeValueAsString(postData);
+			System.out.println(input);
+
+			OutputStream os = conn.getOutputStream();
+			os.write(input.getBytes());
+			os.flush();
+
+			if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+			String output;
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+
+			conn.disconnect();
+
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
